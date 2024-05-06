45ce24640bd0defb145d5f0476324a8a94cb0ea7
Nguyen Tran <nguyentran@MB.local>
2024-05-06 17:22:04 -0400

    add forgetting model

--- af_model.py
+++ af_model.py
@@ -264,27 +264,29 @@ class GPT(nn.Module):
         # start with all of the candidate parameters
         param_dict = {pn: p for pn, p in self.named_parameters()}
         # filter out those that do not require grad
-        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        body_param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad and pn != 'transformer.wte.weight' and pn != 'transformer.wpe.weight'}
         # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
         # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
-        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
-        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
-        optim_groups = [
-            {'params': decay_params, 'weight_decay': weight_decay},
-            {'params': nodecay_params, 'weight_decay': 0.0}
+        body_decay_params = [p for n, p in body_param_dict.items() if p.dim() >= 2]
+        body_nodecay_params = [p for n, p in body_param_dict.items() if p.dim() < 2]
+        body_optim_groups = [
+            {'params': body_decay_params, 'weight_decay': weight_decay},
+            {'params': body_nodecay_params, 'weight_decay': 0.0}
         ]
-        num_decay_params = sum(p.numel() for p in decay_params)
-        num_nodecay_params = sum(p.numel() for p in nodecay_params)
-        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
-        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        num_decay_params = sum(p.numel() for p in body_decay_params)
+        num_nodecay_params = sum(p.numel() for p in body_nodecay_params)
+        print(f"num body decayed parameter tensors: {len(body_decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num body non-decayed parameter tensors: {len(body_nodecay_params)}, with {num_nodecay_params:,} parameters")
         # Create AdamW optimizer and use the fused version if it is available
         fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
         use_fused = fused_available and device_type == 'cuda'
         extra_args = dict(fused=True) if use_fused else dict()
-        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+
+
+        body_optimizer = torch.optim.AdamW(body_optim_groups, lr=learning_rate, betas=betas, **extra_args)
         print(f"using fused AdamW: {use_fused}")
 
-        return optimizer
+        return body_optimizer
 
     def estimate_mfu(self, fwdbwd_per_iter, dt):
         """ estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS """
