dd1b443ed475a3dff8bfe71204c9c9c5ad53cda2
Nguyen Tran <nguyentran@MB.local>
2024-05-06 18:34:28 -0400

    add forgetting model

--- active_forgetting_train.py
+++ active_forgetting_train.py
@@ -243,7 +243,7 @@ def get_lr(it):
     return min_lr + coeff * (learning_rate - min_lr)
 
 def get_token_lr(it):
-    it = it % reset_interval
+    token_it = it % reset_interval
     # 1) linear warmup for warmup_iters steps
     if it < warmup_iters:
         return learning_rate * it / warmup_iters
@@ -251,7 +251,7 @@ def get_token_lr(it):
     if it > lr_decay_iters:
         return min_lr
     # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    decay_ratio = (token_it ) / reset_interval
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
     return min_lr + coeff * (learning_rate - min_lr)
