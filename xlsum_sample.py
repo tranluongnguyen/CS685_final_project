abe6f8929e24f5a6fb10dadf38c4b5135dada3a6
Nguyen Tran <nguyentran@MB.local>
2024-05-17 19:30:32 -0400

    add sample file

--- xlsum_sample.py
+++ xlsum_sample.py
@@ -20,7 +20,7 @@ model_path = "/content/drive/MyDrive/personalized_text_gen/scholary/weights/xlsu
 embed_path = "english"
 
 model_path = "/content/drive/MyDrive/Cs685/weights/xlsum_en/ckpt_best_xlsum_en_standard.pt"
-embed_path = "/content/drive/MyDrive/Cs685/weights/vietnamese/only_embed_adapt_vietnamese_noise.pt"
+embed_path = "/content/drive/MyDrive/Cs685/weights/vietnamese/only_embed_adapt_viet_standard.pt"
 # model_path = "/content/drive/MyDrive/personalized_text_gen/scholary/weights/xlsum_en/ckpt_best_xlsum_en_af.pt"
 # embed_path = "/content/drive/MyDrive/personalized_text_gen/scholary/weights/vietnamese/only_embed_adapt_viet_af.pt"
 # model_path = "/content/drive/MyDrive/personalized_text_gen/weights/xlsum_en/ckpt_best_xlsum_en_noise.pt"
@@ -257,6 +257,6 @@ def evaluate_xlsum(lang='english', pred_file='xlsum_en_std_predict.txt', gold_fi
 # evaluate_xlsum(lang='english', pred_file='xlsum_en_std_predict.txt', gold_file='xlsum_en.pkl')
 # evaluate_xlsum(lang='english', pred_file='xlsum_en_af_predict.txt', gold_file='xlsum_en.pkl')
 
-evaluate_xlsum(lang='vietnamese', pred_file='xlsum_af_std_predict.txt', gold_file='xlsum_vi.pkl')
-# evaluate_xlsum(lang='vietnamese', pred_file='xlsum_af_af_predict.txt', gold_file='xlsum_vi.pkl')
+evaluate_xlsum(lang='vietnamese', pred_file='xlsum_vi_std_predict.txt', gold_file='xlsum_vi.pkl')
+# evaluate_xlsum(lang='vietnamese', pred_file='xlsum_vi_af_predict.txt', gold_file='xlsum_vi.pkl')
 # evaluate_xlsum(lang='vietnamese', pred_file='xlsum_vi_noise_predict.txt', gold_file='xlsum_vi.pkl')
